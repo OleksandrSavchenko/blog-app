6e96d25fecd9da82448627896cf94cca88748b7b
OleksandrSavchenko <oleksandr.savchenko3@gmail.com>
2017-04-18 21:44:11 +0300

    Redux thunk fixed.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
-import { thunk } from 'redux-thunk';
+import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 
 import rootReducer from './reducers/rootReducer';
